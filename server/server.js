7eef3a3b128139dd1ddbe3dd879458cc11f217b0
LukeGarrigan <lukegarrigan8@gmail.com>
2019-10-04 17:47:29 +0100

    Added github OAuth

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const socket = require('socket.io');
 const app = express();
 const GameEngine = require("./GameEngine");
+const axios = require('axios')
 
 
 const gameEngine = new GameEngine();
@@ -13,36 +14,72 @@ let io = socket(server);
 
 setInterval(updateGame, 16);
 
+app.get("/success", function (req, res) {
+    console.log(req);
+
+    let authorisationCode = req.query.code;
+
+    const clientId = "0ebd20dd11e06586d4b8";
+    const clientSecret = process.env.clientSecret;
+    let url = `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${authorisationCode}&scope=user`;
+    axios({
+        method: "post",
+        url,
+        headers: {
+            accept: "application/json"
+        }
+    }). then((response) => {
+        console.log(response);
+        const accessToken = response.data.access_token;
+        grabUserData(accessToken);
+    });
+});
+
+
+function grabUserData(accessToken) {
+    axios({
+        method: "get",
+        url: "https://api.github.com/user",
+        headers: {
+            accept: "application/json",
+            Authorization: `token ${accessToken}`
+        }
+    }).then((response) => {
+        console.log(response);
+    }).catch((err) => {
+        console.log(err);
+    })
+}
 
 
 io.sockets.on("connection", socket => {
-  console.log(`New connection ${socket.id}`);
-  gameEngine.createNewPlayer(socket);
+    console.log(`New connection ${socket.id}`);
+    gameEngine.createNewPlayer(socket);
 
-  socket.on("disconnect", () => {
-    io.sockets.emit("disconnect", socket.id);
-    gameEngine.removePlayer(socket.id);
-  });
+    socket.on("disconnect", () => {
+        io.sockets.emit("disconnect", socket.id);
+        gameEngine.removePlayer(socket.id);
+    });
 
-  socket.on("keyPressed", (key) => {
-    if (!gameEngine.correctKeyPressed(key, socket.id)) {
-      if (!isModifierKey(key)) {
-        socket.emit("wrongLetter")
-      }
+    socket.on("keyPressed", (key) => {
+        if (!gameEngine.correctKeyPressed(key, socket.id)) {
+            if (!isModifierKey(key)) {
+                socket.emit("wrongLetter")
+            }
+        }
+    });
+
+
+    function isModifierKey(key) {
+        return key === "Shift" || key === "Control" || key === "Alt"
     }
-  });
-
-
-  function isModifierKey(key) {
-    return key === "Shift" || key === "Control" || key === "Alt"
-  }
 });
 
 
 function updateGame() {
-  gameEngine.updatePlayers();
-  io.sockets.emit("heartbeat", gameEngine.players);
-  io.sockets.emit("sentence", gameEngine.sentence);
+    gameEngine.updatePlayers();
+    io.sockets.emit("heartbeat", gameEngine.players);
+    io.sockets.emit("sentence", gameEngine.sentence);
 }
 
 
