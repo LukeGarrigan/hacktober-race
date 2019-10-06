7eef57bc8f8b191accf0508102f97742da5b1ac9
Victor Torres <vikthort.t@gmail.com>
2019-10-06 16:37:13 -0500

    update player 'Y' and 'startY' properties on updatePlayers method

--- public/PlayersHandler.js
+++ public/PlayersHandler.js
@@ -1,52 +1,54 @@
-import Player from './Player.js'
+import Player from "./Player.js";
 
 export default class PlayersHandler {
-  constructor () {
-    this.players = []
+  constructor() {
+    this.players = [];
   }
 
-  updatePlayers (serverPlayers) {
+  updatePlayers(serverPlayers) {
     for (let i = 0; i < serverPlayers.length; i++) {
-      const playerFromServer = serverPlayers[i]
+      const playerFromServer = serverPlayers[i];
 
-      const existingPlayer = this.playerExists(playerFromServer)
+      const existingPlayer = this.playerExists(playerFromServer);
       if (!existingPlayer) {
-        this.players.push(new Player(playerFromServer))
+        this.players.push(new Player(playerFromServer));
       } else {
-        existingPlayer.currentIndex = playerFromServer.currentIndex
-        existingPlayer.finished = playerFromServer.finished
-        existingPlayer.winner = playerFromServer.winner
+        existingPlayer.currentIndex = playerFromServer.currentIndex;
+        existingPlayer.finished = playerFromServer.finished;
+        existingPlayer.winner = playerFromServer.winner;
+        existingPlayer.y = playerFromServer.y;
+        existingPlayer.startY = playerFromServer.y;
       }
     }
   }
 
-  playerExists (playerFromServer) {
+  playerExists(playerFromServer) {
     for (let i = 0; i < this.players.length; i++) {
       if (this.players[i].id === playerFromServer.id) {
-        return this.players[i]
+        return this.players[i];
       }
     }
-    return undefined
+    return undefined;
   }
 
-  removePlayer (playerId) {
-    this.players = this.players.filter(player => player.id !== playerId)
+  removePlayer(playerId) {
+    this.players = this.players.filter(player => player.id !== playerId);
   }
 
-  resetPlayers () {
-    this.players = []
+  resetPlayers() {
+    this.players = [];
   }
 
-  draw () {
-    this.players.forEach(player => player.draw())
+  draw() {
+    this.players.forEach(player => player.draw());
   }
 
-  getPlayer (id) {
+  getPlayer(id) {
     for (let i = 0; i < this.players.length; i++) {
       if (this.players[i].id === id) {
-        return this.players[i]
+        return this.players[i];
       }
     }
-    return undefined
+    return undefined;
   }
 }
