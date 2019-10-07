0d692782dcc227afebbde4a6d476b54b6a5d7258
Luke Garrigan <lukegarrigan8@gmail.com>
2019-10-07 20:28:25 +0100

    Merged in master

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const socket = require('socket.io');
 const app = express();
 const GameEngine = require("./GameEngine");
 const axios = require('axios')
-
+const port = 4000;
 
 const gameEngine = new GameEngine();
-const server = app.listen(4000);
+const server = app.listen(port);
 const path = require('path');
 
 const publicDir = path.join(__dirname, '../public');
@@ -14,52 +14,50 @@ app.use(express.static(publicDir));
 
 const io = socket(server);
 
+console.log(`Running on port ${port}`);
+
 setInterval(updateGame, 16);
 
-io.sockets.on('connection', socket => {
-  console.log(`New connection ${socket.id}`);
-  gameEngine.createNewPlayer(socket.id);
 app.get("/success", function (req, res) {
-    console.log(req);
+  console.log(req);
 
-    let authorisationCode = req.query.code;
+  let authorisationCode = req.query.code;
 
-    const clientId = "0ebd20dd11e06586d4b8";
-    const clientSecret = process.env.clientSecret;
-    let url = `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${authorisationCode}&scope=user`;
-    axios({
-        method: "post",
-        url,
-        headers: {
-            accept: "application/json"
-        }
-    }). then((response) => {
-        console.log(response);
-        const accessToken = response.data.access_token;
-        grabUserData(accessToken);
-    });
+  const clientId = "0ebd20dd11e06586d4b8";
+  const clientSecret = process.env.clientSecret;
+  let url = `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${authorisationCode}&scope=user`;
+  axios({
+    method: "post",
+    url,
+    headers: {
+      accept: "application/json"
+    }
+  }).then((response) => {
+    console.log(response);
+    const accessToken = response.data.access_token;
+    grabUserData(accessToken);
+  });
 });
 
-
 function grabUserData(accessToken) {
-    axios({
-        method: "get",
-        url: "https://api.github.com/user",
-        headers: {
-            accept: "application/json",
-            Authorization: `token ${accessToken}`
-        }
-    }).then((response) => {
-        console.log(response);
-    }).catch((err) => {
-        console.log(err);
-    })
+  axios({
+    method: "get",
+    url: "https://api.github.com/user",
+    headers: {
+      accept: "application/json",
+      Authorization: `token ${accessToken}`
+    }
+  }).then((response) => {
+    console.log(response);
+  }).catch((err) => {
+    console.log(err);
+  })
 }
 
 
 io.sockets.on("connection", socket => {
-    console.log(`New connection ${socket.id}`);
-    gameEngine.createNewPlayer(socket);
+  console.log(`New connection ${socket.id}`);
+  gameEngine.createNewPlayer(socket);
 
   socket.on('disconnect', () => {
     io.sockets.emit('disconnect', socket.id);
@@ -73,14 +71,15 @@ io.sockets.on("connection", socket => {
       }
     }
   });
-
-  function isModifierKey (key) {
-    return key === 'Shift' || key === 'Control' || key === 'Alt';
-  }
 });
 
 
-function updateGame () {
+function isModifierKey(key) {
+  return key === 'Shift' || key === 'Control' || key === 'Alt';
+}
+
+
+function updateGame() {
   gameEngine.updatePlayers();
   io.sockets.emit('heartbeat', gameEngine.players);
   io.sockets.emit('sentence', gameEngine.sentence);
@@ -91,7 +90,7 @@ function updateGame () {
   }
 }
 
-function restartGame () {
+function restartGame() {
   // emit event to reset players
   io.sockets.emit('restart');
   gameEngine.restart();
