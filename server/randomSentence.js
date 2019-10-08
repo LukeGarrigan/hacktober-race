c94fa754e48fa29e475ca30b8ba39373e8eb60e3
Luke Garrigan <lukegarrigan8@gmail.com>
2019-10-08 20:40:57 +0100

    Wanted the sentences a little longer and more varied programming languages

--- server/randomSentence.js
+++ server/randomSentence.js
@@ -1,14 +1,13 @@
 module.exports = () => {
   const sentences = [
     'int main(){printf ("Hi World\\n");return 0;}',
-    'console.log("Hi World")',
-    'print("Hi World")',
-    'echo Hi world',
-    'npm start',
-    'git push',
-    'git add -A',
+    'public static void main(String[] args) {}',
+    'def __init__(self, width, height, color=\'black\'):',
+    'SELECT * FROM programming_languages WHERE syntax=\'dodgy\'',
+    'npm run my-funky-build-script',
+    'find . -type f -name "*.php"',
+    ':(){ :|: & };:',
     'const express = require(\'express\')',
-    'const socket = require(\'socket.io\')' // add or change the list
   ];
   return sentences[Math.floor(Math.random() * sentences.length)];
 };
